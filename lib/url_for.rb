262e3ff6bbe57582262c3307a0e6d76353613f28
arirusso <ari.russo@gmail.com>
2017-01-24 15:32:08 -0500

    hash formatting

--- lib/url_for.rb
+++ lib/url_for.rb
@@ -13,21 +13,19 @@ module ActionDispatch
         # if secure && ssl check is not disabled, convert to full url with https
         if !secure.nil? && !SslRequirement.disable_ssl_check?
           if secure == true || secure == 1 || secure.to_s.downcase == "true"
-            options.merge!({
-              :only_path => false,
-              :protocol => 'https'
-            })
+            options.merge!(
+              only_path: false,
+              protocol: 'https'
+            )
 
             # if we've been told to use different host for ssl, use it
             unless SslRequirement.ssl_host.nil?
-              options.merge! :host => SslRequirement.ssl_host
+              options.merge!(host: SslRequirement.ssl_host)
             end
 
             # make it non-ssl and use specified options
           else
-            options.merge!({
-              :protocol => 'http'
-            })
+            options.merge!(protocol: 'http')
           end
         end
 
@@ -40,7 +38,7 @@ module ActionDispatch
         options = args.first
         if !options[:only_path] && !SslRequirement.non_ssl_host.nil?
           if !(/^https/ =~ (options[:protocol] || @request.try(:protocol)))
-            options.merge! :host => SslRequirement.non_ssl_host
+            options.merge!(host: SslRequirement.non_ssl_host)
           end
         end
 
